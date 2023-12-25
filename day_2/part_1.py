5e9b72126c5312f6dea1adb9d1b5b3b5fcb218a8
Saku Aaltonen <saku.aaltonen12@gmail.com>
2023-12-25 11:41:02 +0200

    First full draft of day 2 part 1

--- day_2/part_1.py
+++ day_2/part_1.py
@@ -1,12 +1,33 @@
+import re
+import sys
 
 N_RED = 12
 N_GREEN = 13
 N_BLUE = 14
 
-def parse_game(file_path: str) -> dict:
-    # TODO: this function
-    game_result = {}
+def parse_game(line: str) -> dict:
+    """
+    Parse a line that contains game results, return point sums
+    """
+    pattern = re.compile(r'\d+\s+(red|green|blue)')
+    game_result = {'red': 0, 'green': 0, 'blue': 0}
+    matches = pattern.finditer(line)
 
+    for match in matches:
+        # Get the actual match string from match object
+        match = match.group()
+        match = match.split(' ')
+        color = match[1]
+        score = 0
+
+        try:
+            score =  int(match[0])
+        except ValueError:
+            # In case of score N/A jump to next
+            continue
+
+        if color in game_result:
+            game_result[color] += score
 
     return game_result
 
@@ -18,15 +39,19 @@ def check_validity(game_result: dict) -> bool:
 
 
 def main():
-    input_file_path = "./input.txt"
+    input_file_path = sys.argv[1]
+    line_cnt = 1
+    valid_sum = 0
 
     with open(input_file_path, 'r') as file:
         for line in file:
             game_result = parse_game(line)
-            if not check_validity(game_result):
-                invalid_sum += game_result['id']
+            if check_validity(game_result):
+                print(f"Valid game ID: {line_cnt}")
+                valid_sum += line_cnt
+            line_cnt += 1
 
-    print(invalid_sum)
+    print(valid_sum)
 
     return 0
 
