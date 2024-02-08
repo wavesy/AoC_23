83dc85d72d2d15e57e35180773fb71d33e997007
Saku Aaltonen <saku.aaltonen12@gmail.com>
2024-02-08 17:19:42 +0200

    create_2d_from_file(): parse input file into a 2d tuple

--- day_3/part_1.py
+++ day_3/part_1.py
@@ -1,7 +1,28 @@
+import sys
+import typing as T
+
+
+def create_2d_from_file(filename: str) -> tuple(tuple):
+    # Collect stripped lines in a list first
+    tuples_list = []
+    with open(filename, 'r') as file:
+        # Reference for line length, to check if each line is same length
+        line_length = len(file[0].rstrip())
+        for line in file:
+            # Check that line length is uniform
+            if len(line) != line_length:
+                raise IndexError("Input file lines should be same length.")
+            line = line.rstrip()
+            tuples_list.append(tuple(line))
+
+    tuples_2d = tuple(tuples_list)
+    return tuples_2d
+
 
 
 def main() -> None:
-    pass
+    input_file_name = sys.argv[1]
+    char_map = create_2d_from_file(input_file_name)
 
 
 if __name__ == "__main__":
